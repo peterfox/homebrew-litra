cfee5707fe9f76c4c50a8cc50f9f9d9e9c9e3b67
Peter Fox <peter.fox@peterfox.me>
2023-10-17 10:35:01 +0100

    Correct hash

--- Formula/litra.rb
+++ Formula/litra.rb
@@ -2,7 +2,7 @@ class Litra < Formula
   desc "a small Logitech Litra Glow control application"
   homepage "https://github.com/peterfox/litra"
   url "https://github.com/peterfox/litra/releases/download/v0.3.0/litra-macos-amd64.tar.gz"
-  sha256 "253e43d3d26be2417ab46a3217a508805c70605ebcc946495908cfb856fb334b"
+  sha256 "e2c3812b58690189243669809138edc29d32878bfa12b15e1e8ac1fc2de4ea1b"
 
   def install
     bin.install "litra"
